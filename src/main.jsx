c535d0e1d4d0c7af60230197d0269ff8c9d7ddde
Jhon Anderson Luna <luna.jhon@correounivalle.edu.co>
2024-10-20 18:01:35 -0500

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login.jsx';
 import Home from './Home.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/Home" element={<Home />} />
