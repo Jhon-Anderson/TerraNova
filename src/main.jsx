f16eb2b78af8db0fd68b92f240c782905ab5426d
Jhon Anderson Luna <luna.jhon@correounivalle.edu.co>
2024-10-21 13:57:13 -0500

    v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/autenticacion/Login';
 import Contaminacion from './pages/problem/contaminacion/Contaminacion';
 import Escasez from './pages/problem/escasez/Escasez';
 import Acidificacion from './pages/problem/acidificacion/Acidificacion';
+import Login from './pages/autenticacion/Login';
+import Home from './pages/Home';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} /> 
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/contaminacion" element={<Contaminacion />} />
         <Route path="/escasez" element={<Escasez />} />
