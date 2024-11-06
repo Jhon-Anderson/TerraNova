33ca355604bd1ee39a00a6b6d30e5496ef9546cd
John Alvarez Acevedo <alvarez.john@correounivalle.edu.co>
2024-11-05 22:35:58 -0500

    modificación del canvas para caballo de mar

--- src/modelos-3d/SeaHorseModel.jsx
+++ src/modelos-3d/SeaHorseModel.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useGLTF } from '@react-three/drei';
 
 const SeaHorseModel = () => {
     const { scene } = useGLTF('/model-3d/Sea_horse.glb');
-    
+    const [position, setPosition] = useState([0.04, -0.02, 4.7]);
+
+    // Maneja las pulsaciones de teclas para mover el modelo
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            setPosition((prevPosition) => {
+                switch (event.key) {
+                    case 'ArrowUp':
+                        return [prevPosition[0], prevPosition[1] + 0.1, prevPosition[2]];
+                    case 'ArrowDown':
+                        return [prevPosition[0], prevPosition[1] - 0.1, prevPosition[2]];
+                    case 'ArrowLeft':
+                        return [prevPosition[0] - 0.1, prevPosition[1], prevPosition[2]];
+                    case 'ArrowRight':
+                        return [prevPosition[0] + 0.1, prevPosition[1], prevPosition[2]];
+                    default:
+                        return prevPosition;
+                }
+            });
+        };
+
+        // Agregar y eliminar el evento de teclado
+        window.addEventListener('keydown', handleKeyDown);
+        return () => window.removeEventListener('keydown', handleKeyDown);
+    }, []);
+
     return (
-        <primitive object={scene} scale={0.5} position={[0.1, -0.04, 4.7]}  rotation={[0, Math.PI / -2, 0]}/>
+        <primitive object={scene} scale={0.5} position={position} />
     );
 };
 
