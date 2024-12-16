c33eabe0db5052d1938c0222b9d7891ac66ee8b0
Jhon Anderson Luna <luna.jhon@correounivalle.edu.co>
2024-12-16 13:22:22 -0500

    back quiz

--- src/pages/problem/contaminacion/QuizContaminacion.jsx
+++ src/pages/problem/contaminacion/QuizContaminacion.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate
 import './QuizContaminacion.css';
 
 const QuizContaminacion = () => {
   const filas = Array.from({ length: 5 }, (_, i) => i); // Genera un array [0, 1, 2, 3, 4]
+  const navigate = useNavigate(); // Inicializa useNavigate
+
+  const handleBack = () => {
+    navigate(-1); // Navega a la página anterior
+  };
 
   return (
     <div className="quiz-container">
-      <div className="label-container">
-        <div className="label">Contaminación</div>
-      </div>
-      <table className="tabla">
-        <tbody>
-          {filas.map((fila) => (
-            <tr key={fila}>
-              {filas.map((columna) => (
-                <td key={columna} className="cuadro"></td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="label-container">
-        <div className="label">Medio Ambiente</div>
+      <button className="back-button" onClick={handleBack}>
+        Back
+      </button>
+      <div className="content">
+        <div className="label-container">
+          <div className="label">Contaminación</div>
+        </div>
+        <table className="tabla">
+          <tbody>
+            {filas.map((fila) => (
+              <tr key={fila}>
+                {filas.map((columna) => (
+                  <td key={columna} className="cuadro"></td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="label-container">
+          <div className="label">Medio Ambiente</div>
+        </div>
       </div>
     </div>
   );
