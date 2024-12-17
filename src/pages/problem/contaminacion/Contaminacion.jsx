9d8a7d3dc3ea3a8c18d29989d74ce39d14ca21e2
Jhon Anderson Luna <luna.jhon@correounivalle.edu.co>
2024-12-16 19:33:39 -0500

    images contaminacion

--- src/pages/problem/contaminacion/Contaminacion.jsx
+++ src/pages/problem/contaminacion/Contaminacion.jsx
@@ -25,6 +25,7 @@ const Contaminacion = () => {
             <h1 className="title">Contaminación del Agua</h1>
             
             <div className="info-boxes">
+                {/* Caja 1 */}
                 <div className="info-box" onClick={() => openModal(
                     'Causas de la contaminación del agua',
                     <Canvas className="canvas-modal">
@@ -37,7 +38,10 @@ const Contaminacion = () => {
                         La contaminación del agua es causada por vertidos industriales y domésticos,
                         uso de pesticidas y fertilizantes, y derrames de petróleo.
                     </p>
+                    <img src="/images/causas_contaminacion.jpg" alt="Causas del Agua" className="info-image" />
                 </div>
+
+                {/* Caja 2 */}
                 <div className="info-box" onClick={() => openModal(
                     'Efectos de la contaminación del agua',
                     <Canvas className="canvas-modal">
@@ -50,7 +54,10 @@ const Contaminacion = () => {
                         La contaminación del agua daña ecosistemas acuáticos, afecta la salud humana y 
                         provoca la muerte de flora y fauna.
                     </p>
+                    <img src="/images/efectos_contaminacion.avif" alt="Efectos del Agua" className="info-image" />
                 </div>
+
+                {/* Caja 3 */}
                 <div className="info-box" onClick={() => openModal(
                     'Soluciones para la contaminación del agua',
                     <Canvas className="canvas-modal">
@@ -63,9 +70,11 @@ const Contaminacion = () => {
                         La contaminación del agua puede reducirse tratando desechos, usando menos
                         químicos en agricultura y previniendo derrames.
                     </p>
+                    <img src="/images/soluciones_contaminacion.jpg" alt="Soluciones del Agua" className="info-image" />
                 </div>
             </div>
 
+            {/* Modal */}
             {modalOpen && (
                 <div className="modal-overlay" onClick={closeModal}>
                     <div className="modal-content" onClick={(e) => e.stopPropagation()}>
